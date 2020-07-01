3be9d102863e690dcadaf95bf134ca09b79115bb
guardwhy <hxy1625309592@foxmail.com>
2020-07-01 16:46:54 +0800

    DFS

---
+++ 8-Graph/3-DFsearch.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * 宏定义
+ * @return
+ */
+#define MAXNUM 20 // 顶点数最大个数
+#define VRType int // 顶点变量类型
+#define InfoType char // 存储边指针变量类型
+#define VertexType int // 顶点数据类型
+int main() {
+    printf("hello world!!!");
+    return 0;
+}
